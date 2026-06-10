f5ea2ce831d8c0bdceca8f249b24e288b6068b6a
amirkhan1092 <amirkhan1092@gmail.com>
2026-06-10 10:03:30 +0530

    All Commit

---
+++ 02-Java-Core-OOP/HelloWorld.java
@@ -0,0 +1,8 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("shantanu");
+        System.out.print((char)65+"d"+(char)105+"t"+"y"+(char)65);
+    }
+}
